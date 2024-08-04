b1b9d79efcd199cd08f8ebaef4fa698b8dbd8471
Jonathan Costa <jhoncosta2@gmail.com>
2024-08-04 16:14:00 -0300

    resolução exercícios 7

--- data_engineering/aula_2/exercicios/teste.py
+++ data_engineering/aula_2/exercicios/teste.py
@@ -4,14 +4,40 @@
 # if palavra[-1] == '.'or ',' or '?' or '!':
 #     print(len(palavra) - 1)
 
-frase_2 = 'Na minha terra tem palmeiras onde canta o sabiá.'
+# frase_2 = 'Na minha terra tem palmeiras onde canta o sabiá.'
 
-lista_frase = frase_2.split()
-for palavra in lista_frase:
-    # if len(palavra) == 1:
-    #     continue
-    if palavra[-1] == '.' or ',' or '?' or '!':
-        for letra in palavra:
-           nova_palavra = 
-    else:
-        tamanho = len(palavra)
+# lista_frase = frase_2.split()
+# for palavra in lista_frase:
+#     # if len(palavra) == 1:
+#     #     continue
+#     if palavra[-1] == '.' or ',' or '?' or '!':
+#         for letra in palavra:
+#            nova_palavra = 
+#     else:
+#         tamanho = len(palavra)
+
+# def retorna():
+#     a = 'luiz'
+#     b = [1 , 2 , 3, 4]
+#     return a, b 
+
+# dicionario = {}
+# chave, valor = retorna()
+# print(chave)
+# print(valor)
+# dicionario.update({chave: valor})
+# print(dicionario)
+
+
+#def buscar(dicionario, nome):
+
+contatos = { #para testes
+    "Alice": "1234-5678",
+    "Bob": ["2345-6789", "2345-6789", "2345-6789", "2345-6789"],
+    "Carlos": ["3456-7890"],
+    "Diana": "4567-8901",
+    "Eva": "5678-9012"
+}
+
+print(contatos.get("Bob"))
+
