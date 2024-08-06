4905808f0b9bc03bcc65097d9f992e076e63e571
Jonathan Costa <jhoncosta2@gmail.com>
2024-08-05 23:15:54 -0300

    aula03

---
+++ data_engineering/aula_2/exercicios/ex05.py
@@ -0,0 +1,9 @@
+'''5) faça um código que recebe um elemento a ser encontrado no texto e substitui por outro elemento
+
+pode utilizar o .replace()
+'''
+
+frase = 'Não importa o quão devagar você vá, desde que você não pare. - Confúcio'
+
+frase_modificada = frase.replace('você', 'tu')
+print(frase_modificada)
