5efadcabd37f8c870ba9e9d8a49a545fd6190507
Jonathan Costa <jhoncosta2@gmail.com>
2024-08-19 12:17:36 -0300

    final

---
+++ data_engineering/knn/teste.py
@@ -0,0 +1,10 @@
+frutas = [
+    "maçã", "banana", "laranja", "uva", "morango",
+    "abacaxi", "maçã", "manga", "banana", "kiwi",
+    "melancia", "uva", "mamão", "laranja", "morango",
+    "pera", "abacaxi", "maçã", "cereja", "banana"
+]
+
+print(frutas.count('banana'))
+    
+
